cb65b5d5c5cd13f9a57596c12a5d55f4f5e00473
busrakk <busrakartal160@gmail.com>
2023-04-15 02:32:55 +0300

    add-mui-card

--- src/layouts/admin/MasterLayout.js
+++ src/layouts/admin/MasterLayout.js
@@ -5,21 +5,20 @@ import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import Sidenav from "./Sidenav";
 import Box from "@mui/material/Box";
+import "../../assets/css/adminDashboard.css";
 
 const MasterLayout = () => {
   return (
-    <>
+    <div className="bgcolor">
       <Navbar />
-      <Box height={30} />
+      <Box height={70} />
       <Box sx={{ display: "flex" }}>
         <Sidenav />
         {/* <Sidebar />  */}
-        <div>
           <Outlet />
-        </div>
         {/* <Footer /> */}
       </Box>
-    </>
+    </div>
   );
 };
 
